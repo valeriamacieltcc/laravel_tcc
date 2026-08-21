abeee356b6547f715da561ccfc24dbafe1622d76
gehlisboa <gehmoraeslisboa@gmail.com>
2026-08-21 18:21:27 -0300

    verificar anamnese css

--- resources/views/cliente/perfil/anamnese/ficha.blade.php
+++ resources/views/cliente/perfil/anamnese/ficha.blade.php
@@ -1,25 +1,39 @@
 <!DOCTYPE html>
-
 <html lang="pt-br">
+
 <head>
+
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Perfil da Cliente</title>
 
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0"
+    >
 
+    <title>Anamnese</title>
 
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Parisienne&display=swap" rel="stylesheet">
-
+    <!-- GOOGLE FONTS -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display+SC:wght@400;700&display=swap" rel="stylesheet">
+
+    <link
+        href="https://fonts.googleapis.com/css2?family=Parisienne&family=Playfair+Display+SC:wght@400;500;600&display=swap"
+        rel="stylesheet"
+    >
+
+    <!-- BOOTSTRAP -->
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+    >
+
+    <!-- CSS DO PERFIL -->
+    <link
+        rel="stylesheet"
+        href="{{ asset('css/home.css') }}"
+    >
 
 </head>
-
-<link rel="stylesheet" href="{{ asset('css/home.css') }}">
-
 
 
 <body>
